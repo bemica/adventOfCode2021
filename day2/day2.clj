1f00c3fa881f476a2fe5628a852f4b317d307cc4
Mica Beneke <14852919+bemica@users.noreply.github.com>
2021-12-02 15:10:57 +0100

    Day 2 part 1 solution

---
+++ day2/day2.clj
@@ -0,0 +1,25 @@
+(ns day2
+  (:gen-class))
+
+(use '[clojure.string :only [index-of]])
+
+;; process-file returns a seq of strings in the given file separated by line
+(defn process-file [file-name]
+   (with-open [rdr (clojure.java.io/reader file-name)]
+   (reduce conj [] (line-seq rdr))))
+
+(defn parse-int [n-str]
+  (Integer/parseInt n-str))
+
+(defn pilot [directions x y]
+  (if (= (count directions) 0) [x y]
+  (let [dir (subs (first directions) 0 (index-of (first directions) " "))
+        n   (parse-int (subs (first directions) (+ 1 (index-of (first directions) " "))))]
+    (cond
+      (= dir "forward") (pilot (rest directions) (+ x n) y)
+      (= dir "up") (pilot (rest directions) x (- y n))
+      (= dir "down") (pilot (rest directions) x (+ y n))))
+))
+
+(println (pilot (process-file "day2/input.txt") 0 0))
+(println (reduce * (pilot (process-file "day2/input.txt") 0 0)))
