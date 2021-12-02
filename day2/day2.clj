2d4891a3dcc325d20bf93980430dcb2607a09748
Mica Beneke <14852919+bemica@users.noreply.github.com>
2021-12-02 15:17:23 +0100

    Day 2 part 2 solution

--- day2/day2.clj
+++ day2/day2.clj
@@ -21,5 +21,18 @@
       (= dir "down") (pilot (rest directions) x (+ y n))))
 ))
 
+(defn pilot-aim [directions x y a]
+  (if (= (count directions) 0) [x y]
+  (let [dir (subs (first directions) 0 (index-of (first directions) " "))
+        n   (parse-int (subs (first directions) (+ 1 (index-of (first directions) " "))))]
+    (cond
+      (= dir "forward") (pilot-aim (rest directions) (+ x n) (+ y (* a n)) a)
+      (= dir "up") (pilot-aim (rest directions) x y (- a n))
+      (= dir "down") (pilot-aim (rest directions) x y (+ a n)))
+)))
+
 (println (pilot (process-file "day2/input.txt") 0 0))
 (println (reduce * (pilot (process-file "day2/input.txt") 0 0)))
+
+(println (pilot-aim (process-file "day2/input.txt") 0 0 0))
+(println (reduce * (pilot-aim (process-file "day2/input.txt") 0 0 0)))
