727ead042c16938ec168b2a32c0f41ad0ea3e28f
Mica Beneke <14852919+bemica@users.noreply.github.com>
2021-12-03 12:06:02 +0100

    Solution day 3 part 2

--- day3/day3.clj
+++ day3/day3.clj
@@ -17,11 +17,40 @@
 (defn get-bit [len comp-op bit-count]
   (if (comp-op bit-count (* len 1/2)) 1 0))
 
+(defn x-common-bit [comp-op pos count-one count-zero lines]
+  (cond
+    (= 0 (count lines)) (if (comp-op count-one count-zero) \1 \0)
+    (= \1 (get (first lines) pos)) (x-common-bit comp-op pos (+ count-one 1) count-zero (rest lines))
+    (= \0 (get (first lines) pos)) (x-common-bit comp-op pos count-one (+ count-zero 1) (rest lines))))
+
+(defn most-common-bit [pos count-one count-zero lines]
+  (x-common-bit >= pos count-one count-zero lines))
+
+(defn least-common-bit [pos count-one count-zero lines]
+  (x-common-bit < pos count-one count-zero lines))
+
+(defn filter-bits [common-bit pos all-lines line]
+  (=
+    common-bit
+    (get line pos)))
+
+(defn filter-all [lines pos bit-matcher]
+  (loop [pos 0
+         rem-lines lines]
+    (if (= 1 (count rem-lines)) (first rem-lines)
+      (recur (+ 1 pos) (filter (partial filter-bits (bit-matcher pos 0 0 rem-lines) pos rem-lines) rem-lines))
+    )
+  ))
+
 (let [lines    (process-file "day3/input.txt")
       len      (count lines)
       sum-bits (reduce sum-one-bits (repeat (count (first lines)) 0) lines)]
   (println (*
     (Integer/parseInt (reduce str (map (partial get-bit len >) sum-bits)) 2)
     (Integer/parseInt (reduce str (map (partial get-bit len <) sum-bits)) 2)
+  ))
+  (println (*
+    (Integer/parseInt (filter-all lines 0 most-common-bit) 2)
+    (Integer/parseInt (filter-all lines 0 least-common-bit) 2)
   ))
 )
